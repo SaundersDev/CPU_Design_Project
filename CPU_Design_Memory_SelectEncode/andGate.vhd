1643b2aacbef01a3ba8105bfe2deef96d9138169
nulldoesnotapply <30811001+nulldoesnotapply@users.noreply.github.com>
2018-03-27 21:44:45 -0400

    All hail the glorious john bowden
    Praise be praise be! The and gate works

--- CPU_Design_Memory_SelectEncode/andGate.vhd
+++ CPU_Design_Memory_SelectEncode/andGate.vhd
@@ -5,7 +5,8 @@ library work;
  
 entity andGate is
 	port(
-		A, B	 	: in std_logic_vector(31 downto 0);
+		A	:in std_logic;
+		B	 	: in std_logic_vector(31 downto 0);
 		S			: out std_logic_vector(31 downto 0)
 	);
 end andGate;
@@ -13,10 +14,10 @@ end andGate;
 architecture behaviour of andGate is
  
 begin
-	process(A)
+	process(A, B)
 	begin
 		for I in 0 to 31 loop
-			S(I) <= A(I) and B(I);
+			S(I) <= A and B(I);
 		end loop;
 	end process;
 end behaviour;
--- CPU_Design_Memory_SelectEncode/andGate_tb.vhd
+++ CPU_Design_Memory_SelectEncode/andGate_tb.vhd
@@ -9,13 +9,13 @@ architecture behaviour of andGate_tb is
 
 component andGate is
 	port(
-		A		: in std_logic_vector(31 downto 0);
+		A		: in std_logic;
 		B	 	: in std_logic_vector(31 downto 0);
 		S		: out std_logic_vector(31 downto 0)
 	);
 end component;
  
-signal A_tb : std_logic_vector(31 downto 0);
+signal A_tb : std_logic;
 signal B_tb : std_logic_vector(31 downto 0);
 signal S_tb : std_logic_vector(31 downto 0); 
 begin
@@ -29,13 +29,13 @@ begin
 	begin
 	
 		wait for 1 ns;
-		A_tb <= x"00000003";
+		A_tb <= '1';
 		B_tb <= x"0000000f";
 		wait for 9 ns;		
-		A_tb <= x"00000101";
+		A_tb <= '1';
 		B_tb <= x"0000ff0f";
 		wait for 10 ns;
-		A_tb <= x"0000010f";
+		A_tb <= '0';
 		B_tb <= x"000000ff";
 		wait for 10 ns;
 		
